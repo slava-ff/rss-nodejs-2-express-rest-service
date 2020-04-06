9848f76326d77317fbec03029ecc3ef2d351fe76
s.famin <s.famin@godeltech.com>
2020-04-07 00:35:28 +0300

    refactor: my own server file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,25 @@
+const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const path = require('path');
+const YAML = require('yamljs');
+const userRouter = require('./resources/users/user.router');
+const boardRouter = require('./resources/boards/board.router');
 const { PORT } = require('./common/config');
-const app = require('./app');
+const taskRouter = require('./resources/tasks/task.router');
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
+module.exports = express()
+  .use(express.json())
+  .use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+  .use('/', (req, res, next) => {
+    if (req.originalUrl === '/') {
+      res.send('Service is running!');
+      return;
+    }
+    next();
+  })
+  .use('/users', userRouter)
+  .use('/boards', boardRouter)
+  .use('/tasks', taskRouter)
+  .listen(PORT, () => console.log(`Server started on :${PORT}`));
