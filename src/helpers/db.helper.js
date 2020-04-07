3bad5c6e8b7111fd1a74706b379665dd5b3f7092
s.famin <s.famin@godeltech.com>
2020-04-07 05:38:38 +0300

    refactor: clean db and db helper

--- src/helpers/db.helper.js
+++ src/helpers/db.helper.js
@@ -1,16 +1,3 @@
-// const fs = require('fs').promises;
-// const path = require('path');
-
-// const readDb = async () => {
-//   const db = await fs.readFile(path.join(__dirname, '../db/db.json'), 'utf-8');
-//   return JSON.parse(db);
-// };
-
-// const writeDb = async dbUpdated => {
-//   const db = JSON.stringify(dbUpdated);
-//   await fs.writeFile(path.join(__dirname, '../db/db.json'), db, 'utf-8');
-// };
-
 let db = {
   users: {},
   tasks: {},
@@ -18,12 +5,10 @@ let db = {
 };
 
 const readDb = async () => {
-  console.log('===>>: readDb -> db', db);
   return db;
 };
 
 const writeDb = async dbUpdated => {
-  console.log('===>>: dbUpdated', dbUpdated);
   db = dbUpdated;
 };
 
