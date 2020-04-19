f39b65ba849ef2c07b56e81b035546a0828b2378
s.famin <s.famin@godeltech.com>
2020-04-20 02:32:00 +0300

    refactor: change notification

--- src/helpers/db.helper.js
+++ src/helpers/db.helper.js
@@ -11,7 +11,7 @@ const connectToDb = startServer => {
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', () => {
     console.log('Database is connected');
-    db.dropDatabase(console.log('DB is dropped'));
+    db.dropDatabase(console.log('DB is successfully dropped'));
     startServer();
   });
 };
